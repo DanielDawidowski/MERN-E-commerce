b9c556f09b9b9b5eb647effa4e1d03625141c7e1
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-05 14:53:59 +0200

    User Signup, Signin and Singout

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const expressValidator = require('express-validator')
+
 
 require('dotenv').config()
 
@@ -11,6 +16,12 @@ const app = express()
 
 //db
 mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => console.log("DB connected"));
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
 
 // routes
 app.use("/api", userRoutes)
