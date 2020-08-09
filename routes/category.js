a2d17eb88e089f3efe9beea899aff3569f5e430c
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-09 08:56:49 +0200

    Category update, delete and get all (list)

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const { create, categoryById, read } = require('../controllers/category')
+const { create, categoryById, read, update, remove, list } = require('../controllers/category')
 const { userById } = require('../controllers/user')
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth')
 
 router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin ,create)
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update)
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove)
+router.get('/categories', list)
+
 
 router.param('categoryId', categoryById)
 router.param("userId", userById)
