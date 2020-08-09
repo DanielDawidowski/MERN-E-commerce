427404bec3f2efb845eb0dd03d3e32d16bebaeba
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-09 08:38:33 +0200

    Category by id and single category

--- routes/category.js
+++ routes/category.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { create } = require('../controllers/category')
+const { create, categoryById, read } = require('../controllers/category')
 const { userById } = require('../controllers/user')
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth')
 
-
+router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin ,create)
 
-
+router.param('categoryId', categoryById)
 router.param("userId", userById)
 
 
