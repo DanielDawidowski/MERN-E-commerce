dec2c258b6fd6fcd30d47471cb81ee0c8c1b9356
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-12 14:00:52 +0200

    Set filters with category

--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -5,6 +5,9 @@ import { getCategories } from "./ApiCore";
 import Checkbox from "./Checkbox";
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] },
+  });
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
 
@@ -24,6 +27,9 @@ const Shop = () => {
 
   const handleFilters = (filters, filterBy) => {
     console.log("SHOP", filters, filterBy);
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
   };
 
   return (
@@ -42,7 +48,7 @@ const Shop = () => {
             />
           </ul>
         </div>
-        <div className="col-8">right</div>
+        <div className="col-8">{JSON.stringify(myFilters)}</div>
       </div>
     </Layout>
   );
