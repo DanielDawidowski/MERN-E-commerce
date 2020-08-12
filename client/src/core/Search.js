3e411ff35b265bd9001351062200eed060504827
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-12 21:23:59 +0200

    Backend implementation of search

--- client/src/core/Search.js
+++ client/src/core/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getCategories, list } from "./ApiCore";
+import Card from "./Card";
 
 const Search = () => {
   const [data, setData] = useState({
@@ -50,25 +51,49 @@ const Search = () => {
     setData({ ...data, [name]: event.target.value, searched: false });
   };
 
+  const searchMessage = (searched, results) => {
+    if (searched && results.length > 0) {
+      return `Found ${results.length} products`;
+    }
+    if (searched && results.length < 1) {
+      return `No products found`;
+    }
+  };
+
+  const searchedProducts = (results = []) => {
+    return (
+      <div>
+        <h2 className="mt-4 mb-4">{searchMessage(searched, results)}</h2>
+
+        <div className="row">
+          {results.map((product, i) => (
+            <Card key={i} product={product} />
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   const searchForm = () => (
     <form onSubmit={searchSubmit}>
       <span className="input-group-text">
         <div className="input-group input-group-lg">
           <div className="input-group-prepend">
             <select className="btn mr-2" onChange={handleChange("category")}>
-              <option value="All">Pick Category</option>
-              {categories.map((category, i) => (
-                <option key={i} value={category._id}>
-                  {category.name}
+              <option value="All">All</option>
+              {categories.map((c, i) => (
+                <option key={i} value={c._id}>
+                  {c.name}
                 </option>
               ))}
             </select>
           </div>
+
           <input
-            type="text"
+            type="search"
             className="form-control"
-            placeholder="Search by name"
             onChange={handleChange("search")}
+            placeholder="Search by name"
           />
         </div>
         <div className="btn input-group-append" style={{ border: "none" }}>
@@ -79,11 +104,9 @@ const Search = () => {
   );
 
   return (
-    <div>
-      <div className="container mb-3">
-        {searchForm()}
-        {JSON.stringify(results)}
-      </div>
+    <div className="row">
+      <div className="container mb-3">{searchForm()}</div>
+      <div className="container-fluid mb-3">{searchedProducts(results)}</div>
     </div>
   );
 };
