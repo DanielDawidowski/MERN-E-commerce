a03d238b908916ce3ce79b1890a2e22bfdc20d52
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-12 14:27:58 +0200

    Filter with price range

--- client/src/core/RadioBox.js
+++ client/src/core/RadioBox.js
@@ -8,16 +8,16 @@ const RadioBox = ({ prices, handleFilters }) => {
     setValue(event.target.value);
   };
 
-  return prices.map((p, i) => (
+  return prices.map((price, i) => (
     <div key={i}>
       <input
         onChange={handleChange}
-        value={`${p._id}`}
-        name={p}
+        value={`${price._id}`}
+        name={price}
         type="radio"
         className="mr-2 ml-4"
       />
-      <label className="form-check-label">{p.name}</label>
+      <label className="form-check-label">{price.name}</label>
     </div>
   ));
 };
