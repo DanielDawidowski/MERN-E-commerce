a9ce650512babbe1b2b8f9d510eb59444855ffe1
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-12 13:50:48 +0200

    Handle categories toggle

--- client/src/core/Checkbox.js
+++ client/src/core/Checkbox.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 const Checkbox = ({ categories }) => {
-  return categories.map((c, i) => (
+  const [checked, setChecked] = useState([]);
+
+  const handleToggle = (category) => () => {
+    // return the first index or -1
+    const currentCategoryId = checked.indexOf(category);
+    const newCheckedCategoryId = [...checked];
+    // if currently checked was not already in checked state > push
+    // else pull/take
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(category);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+  };
+
+  return categories.map((category, i) => (
     <li key={i} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
-      <label className="form-check-label">{c.name}</label>
+      <input
+        type="checkbox"
+        className="form-check-input"
+        value={checked.indexOf(category._id === -1)}
+        onChange={handleToggle(category._id)}
+      />
+      <label className="form-check-label">{category.name}</label>
     </li>
   ));
 };
