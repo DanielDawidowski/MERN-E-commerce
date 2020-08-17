db4497882a458a9bc8edb4b8c00182147db54eea
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-17 15:19:38 +0200

    Conditionally show hide add to cart

--- client/src/core/Cart.js
+++ client/src/core/Cart.js
@@ -5,50 +5,47 @@ import { getCart } from "./cartHelpers";
 import Card from "./Card";
 
 const Cart = () => {
-    const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        setItems(getCart());
-    }, []);
+  useEffect(() => {
+    setItems(getCart());
+  }, []);
 
-    const showItems = items => {
-        return (
-            <div>
-                <h2>Your cart has {`${items.length}`} items</h2>
-                <hr />
-                {items.map((product, i) => (
-                    <Card key={i} product={product} />
-                ))}
-            </div>
-        );
-    };
-
-    const noItemsMessage = () => (
-        <h2>
-            Your cart is empty. <br /> <Link to="/shop">Continue shopping</Link>
-        </h2>
-    );
-
+  const showItems = (items) => {
     return (
-        <Layout
-            title="Shopping Cart"
-            description="Manage your cart items. Add remove checkout or continue shopping."
-            className="container-fluid"
-        >
-            <div className="row">
-                <div className="col-6">
-                    {items.length > 0 ? showItems(items) : noItemsMessage()}
-                </div>
-
-                <div className="col-6">
-                    <p>
-                        show checkout options/shipping address/total/update
-                        quantity
-                    </p>
-                </div>
-            </div>
-        </Layout>
+      <div>
+        <h2>Your cart has {`${items.length}`} items</h2>
+        <hr />
+        {items.map((product, i) => (
+          <Card key={i} product={product} showAddToCartButton={false} />
+        ))}
+      </div>
     );
+  };
+
+  const noItemsMessage = () => (
+    <h2>
+      Your cart is empty. <br /> <Link to="/shop">Continue shopping</Link>
+    </h2>
+  );
+
+  return (
+    <Layout
+      title="Shopping Cart"
+      description="Manage your cart items. Add remove checkout or continue shopping."
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-6">
+          {items.length > 0 ? showItems(items) : noItemsMessage()}
+        </div>
+
+        <div className="col-6">
+          <p>show checkout options/shipping address/total/update quantity</p>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Cart;
