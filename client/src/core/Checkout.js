3adbd094db97810eb82c95ab188bb7c0b3f9a995
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-18 19:01:26 +0200

    Save delivery address of orders

--- client/src/core/Checkout.js
+++ client/src/core/Checkout.js
@@ -60,6 +60,8 @@ const Checkout = ({ products }) => {
     );
   };
 
+  let deliveryAddress = data.address;
+
   const buy = () => {
     setData({ loading: true });
     let nonce;
@@ -87,7 +89,7 @@ const Checkout = ({ products }) => {
               products: products,
               transaction_id: response.transaction.id,
               amount: response.transaction.amount,
-              address: data.address,
+              address: deliveryAddress,
             };
 
             createOrder(userId, token, createOrderData)
