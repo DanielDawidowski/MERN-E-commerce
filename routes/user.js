c77657f52d16936e32a0810f12aea2e4b45bbf08
Daniel Dawidowski <dvds1987@gmail.com>
2020-08-19 16:34:14 +0200

    Get user info for profile update

--- routes/user.js
+++ routes/user.js
@@ -10,8 +10,8 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
     })
 })
 
-router.get('/user/:userId', requireSignin, isAuth, read )
-router.put('/user/:userId', requireSignin, isAuth, update)
+router.get("/user/:userId", requireSignin, isAuth, read )
+router.put("/user/:userId", requireSignin, isAuth, update)
 
 
 router.param("userId",  userById)
